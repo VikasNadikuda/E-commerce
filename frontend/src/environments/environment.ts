44828cc67514d3a662517c0ccdf9be68d5741785
vnadiku1 <vikasnadikuda@gmail.com>
2024-06-14 10:35:22 -0400

    firebase settings changed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,5 @@
 export const environment = {
     firebase : {
-        apiKey: "AIzaSyA8aym7Yc5hBUA__BaanwBlVtBvN-qvLyc",
-        authDomain: "termiantionproject.firebaseapp.com",
-        projectId: "termiantionproject",
-        storageBucket: "termiantionproject.appspot.com",
-        messagingSenderId: "54600087706",
-        appId: "1:54600087706:web:a3be679bdc701cef36c723"
+
       }
 };
